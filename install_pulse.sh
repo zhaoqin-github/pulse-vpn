4d1ae28c99129ef5460cf8917c0d7a8ef4d68227
Qin Zhao <q.zhao@f5.com>
2019-08-05 11:20:32 +0800

    Install pulse vpn client

---
+++ install_pulse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+sudo apt-get -y update
+
+if [ ! -f ps-pulse-linux-9.1r1.0-b21-ubuntu-debian-64-bit-installer.deb ] ; then
+  wget https://github.com/zhaoqin-github/misc/raw/master/deb/ps-pulse-linux-9.1r1.0-b21-ubuntu-debian-64-bit-installer.deb
+fi
+
+sudo dpkg --install ps-pulse-linux-9.1r1.0-b21-ubuntu-debian-64-bit-installer.deb
+sudo patch /usr/local/pulse/PulseClient_x86_64.sh PulseClient_x86_64.sh.patch
+sudo /usr/local/pulse/PulseClient_x86_64.sh install_dependency_packages
