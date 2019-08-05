823d676906bf3438bb18371c3e5c6e5b4eab62b2
Qin Zhao <q.zhao@f5.com>
2019-08-05 13:37:41 +0800

    Ping openstack controller

--- connect.sh
+++ connect.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-/usr/local/pulse/PulseClient_x86_64.sh -h $VPNHOST -u $VPNUSER -p $VPNPASS -r Users
+/usr/local/pulse/PulseClient_x86_64.sh -h $VPN_HOST -u $VPN_USER -p $VPN_PASS -r Users
 
 sleep 10
 
 /usr/local/pulse/PulseClient_x86_64.sh -S
+
+ping -c 3 $OS_HOST
