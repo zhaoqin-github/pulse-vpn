846369261d2a375e5f2b6e45fcb5c96ca4ce33b3
Qin Zhao <q.zhao@f5.com>
2019-08-12 10:49:10 +0800

    Do not overwrite default ca certificates

--- install_pulse.sh
+++ install_pulse.sh
@@ -9,6 +9,6 @@ fi
 sudo dpkg --install ps-pulse-linux-9.1r1.0-b21-ubuntu-debian-64-bit-installer.deb
 sudo patch /usr/local/pulse/PulseClient_x86_64.sh PulseClient_x86_64.sh.patch
 sudo /usr/local/pulse/PulseClient_x86_64.sh install_dependency_packages
-#cat timerwell.juniper.net.crt | sudo tee -a /etc/ssl/certs/ca-certificates.crt
-sudo cp timerwell.juniper.net.crt /etc/ssl/certs/ca-certificates.crt
+cat timerwell.juniper.net.crt | sudo tee -a /etc/ssl/certs/ca-certificates.crt
+#sudo cp timerwell.juniper.net.crt /etc/ssl/certs/ca-certificates.crt
 md5sum /etc/ssl/certs/ca-certificates.crt
