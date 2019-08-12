175c913be754642521f442553db384f53a673a2c
Qin Zhao <q.zhao@f5.com>
2019-08-12 12:20:44 +0800

    Wait for VPN connection

--- connect.sh
+++ connect.sh
@@ -1,11 +1,25 @@
 #!/bin/bash -e
 
+INTERVAL=10
+RETRY=6
+
 /usr/local/pulse/PulseClient_x86_64.sh -h $VPN_HOST -u $VPN_USER -p $VPN_PASS -r Users &
 
-sleep 30
+while [ $RETRY -gt 0 ] ; do
+  sleep $INTERVAL
+  ((RETRY=RETRY-1))
 
-cat ~/.pulse_secure/pulse/pulsesvc.log
+  /usr/local/pulse/PulseClient_x86_64.sh -S | grep "connection status : Connected"
 
-/usr/local/pulse/PulseClient_x86_64.sh -S
+  if [ $? -eq 0 ] ; then
+    RETRY=-1
+  fi
+done
+
+if [ $RETRY -eq 0 ] ; then
+  echo "Fail to connect VPN"
+  cat ~/.pulse_secure/pulse/pulsesvc.log
+  exit 1
+fi
 
 ping -c 3 10.250.13.69
