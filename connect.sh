3d76b68193f147110552a8d9080026214bb292df
Qin Zhao <q.zhao@f5.com>
2019-08-12 10:43:46 +0800

    Sleep 30 seconds

--- connect.sh
+++ connect.sh
@@ -2,10 +2,10 @@
 
 /usr/local/pulse/PulseClient_x86_64.sh -h $VPN_HOST -u $VPN_USER -p $VPN_PASS -r Users &
 
-sleep 10
-
-/usr/local/pulse/PulseClient_x86_64.sh -S
+sleep 30
 
 cat ~/.pulse_secure/pulse/pulsesvc.log
+
+/usr/local/pulse/PulseClient_x86_64.sh -S
 
 ping -c 3 10.250.13.69
